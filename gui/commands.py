9a4f82964bfbe5c43f2a6fb264f0546b28f2417b
Shal1928 <Shal1928@yandex.ru>
2021-01-30 16:14:37 +0700

    ES-1 RWI set data WORK_TYPE

--- gui/commands.py
+++ gui/commands.py
@@ -6,14 +6,23 @@ from package1.db import get_conn_db, try_exec
 
 # def get_widget(form)
 #     tk.Label(form, text=f[0])
+def get_db_data(table):
+    conn = get_conn_db()
+    cur = try_exec(conn, 'SELECT ROWID,* FROM main.{}'.format(table))
+    rows = cur.fetchall()
+    # values = [x[0] for x in liste]
+    conn.close()
+    print(rows)
+    return rows
 
-def get_frame(m, t, entry_type, s):
+
+def get_frame(m, t, entry_type, s, data):
     f = tk.Frame(m)
     label = tk.Label(f, text=t).pack(side=s, anchor=tk.W)
     e = {
         'entry': tk.Entry(f),
         'check': tk.Checkbutton(f),
-        'combo': ttk.Combobox(f)
+        'combo': ttk.Combobox(f, values=[d[1] for d in data])
     }[entry_type].pack(side=s, fill=tk.X)
     return f
 
@@ -23,20 +32,22 @@ def get_rwi_widgets(m):
 
     # cur1 = try_exec(conn, '''SELECT * FROM main.RWI''')
     # rwi_fields = {}
-
+    # combo['values'] = (1, 2, 3, 4, 5, "Текст")
+    # combo.current(1)  # установите вариант по умолчанию
     # opts = {'ipadx': 10, 'ipady': 10, 'fill': tk.BOTH}
     # label_a.pack(side=tk.TOP, **opts)
     # frame1 = Frame(root)
     # frame1.pack(side=TOP, fill=X)
     # is_plan = tk.Frame(w)
     # get_frame(m, '', 'combo').grid(row=0, column=1, padx=10, sticky=tk.W)
+    wt = 'WORK_TYPE'
     return {
-        'IS_PLAN': get_frame(m, 'Плановая?', 'check', tk.TOP).grid(row=0, column=0, padx=10, pady=5, sticky=tk.EW),
-        'WORK_TYPE': get_frame(m, 'Тип работы', 'combo', tk.TOP).grid(row=0, column=1, padx=10, pady=5, sticky=tk.EW),
-        'TSHIRT':  get_frame(m, 'Размер', 'combo', tk.TOP).grid(row=0, column=2, padx=10, pady=5, sticky=tk.EW),
-        'ID': get_frame(m, 'Id', 'entry', tk.TOP).grid(row=0, column=3, padx=10, pady=5, sticky=tk.EW),
-        'TITLE': get_frame(m, 'Заголовок', 'entry', tk.TOP).grid(row=1, column=0, columnspan=4, padx=10, pady=5, sticky=tk.EW),
-        'COMMENT': get_frame(m, 'Комментарий', 'entry', tk.TOP).grid(row=2, column=0, columnspan=4, padx=10, pady=5, sticky=tk.EW)
+        'IS_PLAN': get_frame(m, 'Плановая?', 'check', tk.TOP, []).grid(row=0, column=0, padx=10, pady=5, sticky=tk.EW),
+        wt: get_frame(m, 'Тип работы', 'combo', tk.TOP, get_db_data(wt)).grid(row=0, column=1, padx=10, pady=5, sticky=tk.EW),
+        'TSHIRT':  get_frame(m, 'Размер', 'combo', tk.TOP, []).grid(row=0, column=2, padx=10, pady=5, sticky=tk.EW),
+        'ID': get_frame(m, 'Id', 'entry', tk.TOP, []).grid(row=0, column=3, padx=10, pady=5, sticky=tk.EW),
+        'TITLE': get_frame(m, 'Заголовок', 'entry', tk.TOP, []).grid(row=1, column=0, columnspan=4, padx=10, pady=5, sticky=tk.EW),
+        'COMMENT': get_frame(m, 'Комментарий', 'entry', tk.TOP, []).grid(row=2, column=0, columnspan=4, padx=10, pady=5, sticky=tk.EW)
     }
     # for d in cur1.description:
     #     if d ==
